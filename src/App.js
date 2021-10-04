2cb25dc3d601a35dda98d734ade9e854ff793af3
Siva <byteshiva@users.noreply.github.com>
2021-10-04 07:44:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import Divider from '@material-ui/core/Divider';
 import FadeIn from 'react-fade-in';
-import AppBar from './AppBar';
 import Demo from './Demo';
-import Templates from './Templates';
-import Footer from './Footer';
 
 function App() {
   return (
     <Container maxWidth="md">
       <FadeIn>
-        <AppBar />
         <Divider />
         <div id="demo" />
         <Demo />
         <Divider />
-        <Templates />
-        <Divider />
-        <Footer />
       </FadeIn>
     </Container>
   );
